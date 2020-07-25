3ae3b5184e88907190564306317d5cb075666d81
Adeilson Silva <adeilson@gmail.com>
2020-07-25 10:02:29 -0300

    Marcadores

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,6 +10,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _marcadores = {};
 
   void _onMapCreated(GoogleMapController googleMapController) {
     _controller.complete((googleMapController));
@@ -30,6 +31,49 @@ class _HomeState extends State<Home> {
     );
   }
 
+  void _carregarMarcadores() {
+    Set<Marker> marcadoresLocal = {};
+
+    var marcadorShopping = Marker(
+      markerId: MarkerId('marcador-shopping'),
+      position: LatLng(-22.931738, -47.076785),
+      infoWindow: InfoWindow(
+        title: 'Campinas Shopping',
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+        BitmapDescriptor.hueBlue,
+      ),
+      rotation: 45,
+      onTap: () {
+        print('Shopping clicado');
+      },
+    );
+
+    var marcadorCorreios = Marker(
+      markerId: MarkerId('marcador-correios'),
+      position: LatLng(-22.931175, -47.079596),
+      infoWindow: InfoWindow(
+        title: 'Correios - CEE Campinas',
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+        BitmapDescriptor.hueYellow,
+      ),
+    );
+
+    marcadoresLocal.add(marcadorShopping);
+    marcadoresLocal.add(marcadorCorreios);
+
+    setState(() {
+      _marcadores = marcadoresLocal;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarMarcadores();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,10 +90,11 @@ class _HomeState extends State<Home> {
           mapType: MapType.normal,
           // mapType: MapType.hybrid,
           initialCameraPosition: CameraPosition(
-            target: LatLng(-22.923923, -47.090914),
+            target: LatLng(-22.931738, -47.076785),
             zoom: 16,
           ),
           onMapCreated: _onMapCreated,
+          markers: _marcadores,
         ),
       ),
     );
