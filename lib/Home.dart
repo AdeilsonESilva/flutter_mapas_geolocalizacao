c64dcaf3d08b29e6b960cba086b168019c067385
Adeilson Silva <adeilson@gmail.com>
2020-07-25 09:48:33 -0300

    Movimentos da camera

--- lib/Home.dart
+++ lib/Home.dart
@@ -11,22 +11,45 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
 
+  void _onMapCreated(GoogleMapController googleMapController) {
+    _controller.complete((googleMapController));
+    print(_controller.isCompleted);
+  }
+
+  void _movimentarCamera() async {
+    var googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(-22.939208, -47.064586),
+          zoom: 16,
+          tilt: 45, // Angulo
+          bearing: 30, // Rotação
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapas e geolocalização'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _movimentarCamera,
+        child: Icon(Icons.done),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
+          // mapType: MapType.hybrid,
           initialCameraPosition: CameraPosition(
             target: LatLng(-22.923923, -47.090914),
             zoom: 16,
           ),
-          onMapCreated: (controller) {
-            _controller.complete(controller);
-          },
+          onMapCreated: _onMapCreated,
         ),
       ),
     );
